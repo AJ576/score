1ed4eda291bba476f6398a0d357f358e4a975506
Aditya Jha <aditya.jha2020123@gmail.com>
2026-09-05 13:44:41 -0400

    implemented my own erf and erfc

--- src/statistics/cdf.hpp
+++ src/statistics/cdf.hpp
@@ -11,3 +11,10 @@ namespace std_math
     double cdf_erfc(double x, double mean = 0.0, double stddev = 1.0);
     double cdf_erf(double x, double mean = 0.0, double stddev = 1.0);
 }
+
+namespace my_math
+{
+    double my_erf(double x);
+    double my_erfc(double x);
+    double cdf(double x, double mean = 0.0, double stddev = 1.0);
+} // namespace my_math
